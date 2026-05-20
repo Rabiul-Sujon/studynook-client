156e25c94d84754273031519ae4373ce246c28a5
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-05-21 03:08:05 +0600

    Fixing Axios Mistake

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,16 +1,9 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const axiosInstance = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL,
-//     withCredentials: true,
-// });
+const axiosInstance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
 
-// export default axiosInstance;
+export default axiosInstance;
 
-import axiosInstance from '../api/axiosInstance'; 
-
-const login = async (email, password) => {
-    // Calling axiosInstance automatically injects { withCredentials: true }
-    const response = await axiosInstance.post('/jwt', { email, password }); 
-    return response.data;
-};
